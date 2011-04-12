841d6ad9768e0d46ea9c643d28e577b0775956f6
Chad Q <4r10r5@gmail.com>
2011-04-11 23:56:31 -0700

    Add Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Japanthefuck::Application.routes.draw do
+  get "pages/about"
+
+  get "pages/home"
+
+  get "pages/contact"
+
   resources :posts
 
   resources :users
@@ -52,7 +58,7 @@ Japanthefuck::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   #root :to => "posts"
 
   # See how all your routes lay out with "rake routes"
 
