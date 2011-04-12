d7a7240420b24abb590d31146170417a67f0f8f2
Chad Q <4r10r5@gmail.com>
2011-04-11 23:06:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Japanthefuck::Application.config.secret_token = '9720153bfbd7158dfcd0eec275bea8bcb9cea0084393e9e33c0d52057c9c01d320fe731d469588c3d0f2a833b29855f8ae36cc9a85ffea1e6c4c64f95a08ed1d'
